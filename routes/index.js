bdb537994b682c2bdff15b27cc7f27d1b1dd0407
KoTaeKwang <rhxorhkd@naver.com>
2018-02-18 23:59:51 +0900

    jenkinsTest2

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var db = require('../models/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express_server' });
+  res.render('index', { title: 'Express_yoyo' });
 });
 
 router.get('/test',function(req,res,next){
