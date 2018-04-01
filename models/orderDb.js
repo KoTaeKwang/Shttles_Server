3fda70e2928fb0ffdf1485a2491cc554fd207e7a
KoTaeKwang <rhxorhkd@naver.com>
2018-04-01 18:46:24 +0900

    add code

--- models/orderDb.js
+++ models/orderDb.js
@@ -19,7 +19,7 @@ exports.getOrderList = function(user_id,callback){
         function(callback){
             pool.getConnection(function(err,connection){
                 var coffeeListSql ="select o.order_id, o.state, o.price,o.user_id, c.count,coffee.name from orders AS o JOIN coffee_orders AS c ON o.order_id = c.order_id JOIN coffee on coffee.coffee_id = c.coffee_id where o.user_id = ?  order by o.date DESC"
-              
+                
                 connection.query(coffeeListSql,user_id,function(err,coffeeList){
                     console.log("coffeeList : ",coffeeList);
                     callback(null,coffeeList);              
@@ -30,6 +30,12 @@ exports.getOrderList = function(user_id,callback){
              var obj = [];
              var standard = -1;
              var coffeeTemp=[];
+            
+             if(coffeeList.length==0){
+                 var obj ={"result":"empty"};
+                 callback(null,obj);
+                 return;
+             }
 
             forEach(coffeeList,function(item,index,arr){
                 console.log('index : ',index, "  coffeeList.length : ",coffeeList.length);
@@ -70,6 +76,7 @@ exports.getOrderList = function(user_id,callback){
     })
 
 };
+
 
 
 exports.getOrderDetail = function(order_id,callback){
@@ -145,37 +152,151 @@ exports.getOrderDetail = function(order_id,callback){
 
 
 exports.insertOrder = function (data, callback) {
-    var user_id  = "naver.com"
-    var order_address = "여기다"
-    var order_totalPrice = "3000"
-    var coffee =[
-        {
-            "name" : "아메리카노",
-            "count" : 2,
-            "price" : 3000,
-            "option" : [{"name" : "샷추가"},{"name" : "차갑게"}]
-        },
-        {
-            "name" : "라떼",
-            "count" : 2,
-            "price" : 3000,
-            "option" : [{"name" : "샷추가"},{"name" : "차갑게"}]
-        }
-
-    ]
-    console.log(data.user_id);
-    console.log(data.order_address);
-    console.log(data.order_totalPrice);
-    console.log(data.coffee);
+    console.log("data : " ,data);
+    var user_id  = data.user_id;
+    var order_address =data.order_address
+    var order_totalPrice = data.order_totalPrice
+    var coffee= data.coffee;
+    var food = data.food;
+    var order_id;
+    var coffee_ordersId;
+    var food_ordersId;
+    console.log("userId :" , data.user_id);
+    console.log("address : ",data.order_address);
+    console.log("totalPrice : ",data.order_totalPrice);
+    console.log("Coffee : ",data.coffee);
+    console.log("food : " ,data.food)
     
 
-    // order 먼저  address userId 넣고
-    // coffee_orders 에  count, coffee_id ,order_id,price
-    //coffeeOption_orders 에 추가 
+    pool.getConnection(function(err,connection){
+        if(err) throw err; 
+
+        async.waterfall([
+            function(callback){
+                
+                connection.beginTransaction(function(err){ //orders insert
+                    if(err) throw err;
+        
+                    var addOrdersSql = "insert into orders(state,address,price,user_id) values(?,?,?,?)";
+                
+                    var orders = [
+                     0, order_address, order_totalPrice,user_id
+                    ]
+        
+                    console.log("orders : ",orders);
+        
+                    var query = connection.query(addOrdersSql,orders,function(err,results){
+                       console.log(query.sql);
+                        if(err){return connection.rollback(function(){
+                            throw err;
+                        })
+                    }
+                    order_id = results.insertId;
+                    callback(null,connection);
+                })                 
+            })
+
+            },function(connection,callback){ //coffeeorders insert
+                   
+                forEach(coffee,function(item,index,arr){
+                    var insertCoffeeOrdersSql = "insert into coffee_orders(count,coffee_id,order_id,price) values(?,?,?,?)";
+                    
+                    var coffee_orders = [
+                        coffee[index].count, coffee[index].coffee_id, order_id , coffee[index].price
+                    ]
+
+                    console.log("coffee_orders " , coffee_orders);
+                    
+                    var query = connection.query(insertCoffeeOrdersSql,coffee_orders,function(err,results){
+                        console.log(query.sql);
+                        if(err){return connection.rollback(function(){
+                            throw err;
+                        })
+                     } 
+                   
+                     coffee_ordersId = results.insertId;
+
+                     if(typeof coffee[index].option == "undefined"){
+                         console.log("coffee is undefined");
+                     }
+                    else{
+                     forEach(coffee[index].option,function(item,optionIndex,arr){
+                        var insertCoffeeOptionSql = "insert into coffeeOption_orders(coffee_ordersId,option_id) values(?,?)";
+
+                        var coffee_options = [coffee_ordersId,coffee[index].option[optionIndex].option_id];
+
+                        var query = connection.query(insertCoffeeOptionSql,coffee_options,function(err,results){
+                            console.log(query.sql);
+                            if(err){return connection.rollback(function(){
+                                throw err;
+                            })
+                         } 
+
+                        })
+                    })
+
+                }
+
+                })
+            
+                    if(coffee.length-1 == index)
+                        callback(null,connection);
+                })
+            },function(connection,callback){// foodorders insert
+                if(typeof food == "undefined"){ console.log("food is undefined"); callback(null,connection);}
+                else{
+                forEach(food,function(item,index,arr){
+                    var insertFoodOrdersSql = "insert into food_orders(count,food_id,order_id,price) values(?,?,?,?)";
+                    
+                    var food_orders = [
+                        food[index].count, food[index].food_id, order_id , food[index].price
+                    ]
+
+                    console.log("food_orders " , food_orders);
+                    
+                    var query = connection.query(insertFoodOrdersSql,food_orders,function(err,results){
+                        console.log(query.sql);
+                        if(err){return connection.rollback(function(){
+                            throw err;
+                        })
+                     } 
+                  
+                     food_ordersId = results.insertId;
 
 
+                     forEach(food[index].option,function(item,optionIndex,arr){
+                        var insertFoodOptionSql = "insert into foodOption_orders(food_ordersId,option_id) values(?,?)";
 
-    var obj =[{"success" : "ok"}];
-    callback(obj);
+                        var food_options = [food_ordersId,food[index].option[optionIndex].option_id];
+
+                        var query = connection.query(insertFoodOptionSql,food_options,function(err,results){
+                            console.log(query.sql);
+                            if(err){return connection.rollback(function(){
+                                throw err;
+                            })
+                         } 
+
+                        })
+                    })
+
+                })
+            
+                    if(food.length-1 == index)
+                        callback(null,connection);
+                })
+            }
+        }
+        ],function(err,connection){
+            connection.commit(function(err){
+              if(err)
+                return connection.rollback(function(){
+                    throw err;
+                })  
+            });
+
+            var obj =[{"success" : "ok"}];
+            callback(obj);
+
+        }) 
+    })
 }
-
