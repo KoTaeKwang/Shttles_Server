a9fb29f360244f5229b04b70a4ae04dfc5c832ba
고태광 <kotaekwang@gotaegwang-ui-MacBook-Pro.local>
2018-05-29 00:17:30 +0900

    update character

--- routes/notice.js
+++ routes/notice.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models/noticeDB');
+var db = require('../models/noticeDb');
 var logger = require('../winston');
 
 router.get('/',function(req,res,next){
